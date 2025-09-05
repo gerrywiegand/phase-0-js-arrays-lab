1b89bdc133ea6beeab78ed0deb4915bd5c82f636
Gerry <gerrywiegand@gmail.com>
2025-09-05 18:45:21 -0400

    finished

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,5 +1,30 @@
 // Write your code here
+let products = ['laptop', 'phone', 'headphones', 'monitor']
 
+function logFirstProduct(first) {
+  first= products[0]
+ console.log(first)
+}
+
+function addProduct(name){
+  
+  products.push (name)
+}
+ addProduct("bean")
+ console.log (products)
+
+ function updateProductName (number, id){
+  
+  products[number]= id
+ }
+updateProductName(1, 'smartphone')
+console.log(products)
+
+function removeLastProduct() {
+products.pop()
+}
+removeLastProduct()
+console.log(products)
 
 
 // Export the necessary parts for testing
